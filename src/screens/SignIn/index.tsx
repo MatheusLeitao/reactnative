b3a2a6b024a8928ba363bbb5d50aa67dfba6d7a7
Matheus Leitão <matheus_ramos@outlook.com>
2021-08-02 16:20:18 -0300

    Done Splash icon and signin screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,18 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React  from 'react';
+import { Image, Text, View, StatusBar } from 'react-native';
 import { styles } from './style'
+import IlluastratorImg from '../../assets/illustration.png'
+import {ButtonIcon} from '../../components/ButtonIcon'
 
 export function SignIn() {
 
-    const [text, setText] = useState('Matheus')
 
     return (
         <View style={styles.container}>
-            <Text>Chill daddy!</Text>
-            <StatusBar style="auto" />
-            <TextInput style={styles.input} onChangeText={(value) => setText(value)} />
-            <Text>You've typed: {text}</Text>
+            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
+            <Image source={IlluastratorImg} style={styles.image} resizeMode="stretch" />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {'\n'}
+                    suas jogatinas {'\n'}
+                    facilmente
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {'\n'}
+                    favoritos com seus amigos
+                </Text>
+                <ButtonIcon title="Entrar com Discord" activeOpacity={0.6}/>
+            </View>
         </View>
     );
 }

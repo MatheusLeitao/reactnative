c95fb123c75bb6ded7bf22d28c9a73d6d93fac65
MatheusLeitao <matheus_ramos@outlook.com>
2021-08-03 00:49:27 -0300

    Home header finished

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,10 +3,16 @@ import { Image, Text, View, StatusBar } from 'react-native';
 import { styles } from './style'
 import IlluastratorImg from '../../assets/illustration.png'
 import {ButtonIcon} from '../../components/ButtonIcon'
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
 
+    const navigation = useNavigation();
 
+    function handleSignIn() {
+        // @ts-ignore
+        navigation.navigate('Home')
+    }
     return (
         <View style={styles.container}>
             <Image source={IlluastratorImg} style={styles.image} resizeMode="stretch" />
@@ -21,7 +27,7 @@ export function SignIn() {
                     Crie grupos para jogar seus games {'\n'}
                     favoritos com seus amigos
                 </Text>
-                <ButtonIcon title="Entrar com Discord" activeOpacity={0.6} />
+                <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} rippleColor="#d8798c"/>
             </View>
         </View>
     );

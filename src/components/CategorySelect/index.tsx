1fb3fd81b9281f14fb13bf7d2915a7f091770034
MatheusLeitao <matheus_ramos@outlook.com>
2021-08-03 07:26:39 -0300

    svg

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { ScrollView } from "react-native";
+import { styles } from "./styles";
+
+
+export function CategorySelect(){
+    return(
+        <ScrollView
+            horizontal
+            style={styles.container}
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{paddingRight: 40}}
+        />
+    )
+}
